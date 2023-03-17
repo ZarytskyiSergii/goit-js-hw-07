852a3fcec8f53c04062a6b5cbdeb46263ecf331a
Sergii <sonik2552@gmail.com>
2023-03-17 14:00:10 +0100

    Iteration1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const items = galleryItems.map(item => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}" >
+  <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+  />
+</a>
+</div>`}).join("");
+
+const galleryBox = document.querySelector('.gallery');
+galleryBox.innerHTML = items;
+
+galleryBox.addEventListener('click', onLinkClick);
+
+function onLinkClick(event) {
+    event.preventDefault();
+	const instance = basicLightbox.create(`
+		<img width="1400" height="900" src="${event.target.dataset.source}">
+	`)
+	instance.show()
+}
+
 console.log(galleryItems);
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+gallery.innerHTML = galleryItems.map(({ preview, original, description }) => {
+    return `
+    <li>
+    <a class='gallery__item' href="${original}">
+    <img class="gallery__image" src="${preview}" alt="$description" />
+    </a></li>`;
+})
+    .join("");
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelaay: 250,
+    })
+
 console.log(galleryItems);
